d0d8423dd9916f5f3551ee7b7786434d431a591f
Elliot Koh <appdev.moose@gmail.com>
2019-12-30 13:09:27 +0800

    Added player init in game manager

--- src/managers/gamemanager.js
+++ src/managers/gamemanager.js
@@ -20,18 +20,36 @@ export var JoinRoom = (namespace)=>{
     socket.on('GAME_STATE', (state)=>OnReceiveGameState(state));
 }
 
-export var OnReceiveGameState = (state)=>{    
-    let new_players = []
-    for(let player of state.players)
-    {
-        new_players.push({
-            type: 'player',
-            position: player.position,
-            renderer: Player
-        })
+export var OnReceiveGameState = (state)=>{        
+    for(let new_state_player of state.players) {
+        //find the equivalent player to update by their id
+        let player_index_ref = players.findIndex((e)=>e.id == new_state_player.id);
+        
+        //if this player has never been rendered -> we must add them into the array for the first time
+        if(player_index_ref == -1) {            
+            new_state_player.lerp = [];//lerp object array
+            players.push(new_state_player);
+        }
+        else {
+            //has been rendered before, so we append the updates into the lerp array
+            let player = players[player_index_ref];
+            let lerp_package = {
+                position: new_state_player.position,
+                timestamp: Date.now()
+            }
+            
+            player.lerp.push(lerp_package);
+            player.max_stamina = new_state_player.max_stamina;
+            player.current_stamina = new_state_player.current_stamina;
+            player.sprint_speed = new_state_player.sprint_speed;
+            player.default_speed = new_state_player.default_speed;
+            player.current_speed = new_state_player.current_speed;
+            player.size = new_state_player.size;
+            player.reach = new_state_player.reach;
+            player.action = new_state_player.action;
+            player.sprint = new_state_player.sprint;   
+        }        
     }
-
-    players = new_players;            
 }
 
 export var SendControls = (controls)=>{    
