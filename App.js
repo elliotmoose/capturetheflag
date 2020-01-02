50cdc93335614316b04b762fa1013613262b7698
Elliot Koh <appdev.moose@gmail.com>
2020-01-03 00:46:45 +0800

    Added menu to specify ip address

--- App.js
+++ App.js
@@ -6,165 +6,191 @@
  * @flow
  */
 
-import React, {Component} from 'react';
+import React, { Component, useState } from 'react';
 // import {} from 'react-native-socketio'
-import {GameEngine} from 'react-native-game-engine';
+import { GameEngine } from 'react-native-game-engine';
 
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  TouchableOpacity,
-  StatusBar,
-  Dimensions,
-  Image,
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    TouchableOpacity,
+    StatusBar,
+    Dimensions,
+    Image,
+    TextInput,
 } from 'react-native';
 
 import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
+    Header,
+    LearnMoreLinks,
+    Colors,
+    DebugInstructions,
+    ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-import {PlayerSystem} from './src/systems/PlayerSystem';
+import { PlayerSystem } from './src/systems/PlayerSystem';
 import Joystick from './src/renderers/Joystick';
-import {  
-  SendControls,
-  JoinRoom,
-  players,
-  InitializeSocketIO,
-  FindMatch,
+import {
+    SendControls,
+    JoinRoom,
+    players,
+    InitializeSocketIO,
+    FindMatch,
 } from './src/managers/gamemanager';
 import Player from './src/renderers/Player';
 import Button from './src/renderers/controls/Button';
-import {JoystickSystem} from './src/systems/JoystickSystem';
-import {ButtonsSystem} from './src/systems/ButtonsSystem';
-import {ControlsSystem} from './src/systems/ControlsSystem';
+import { JoystickSystem } from './src/systems/JoystickSystem';
+import { ButtonsSystem } from './src/systems/ButtonsSystem';
+import { ControlsSystem } from './src/systems/ControlsSystem';
 import Images from './src/assets/Images';
-import {CameraSystem} from './src/systems/CameraSystem';
+import { CameraSystem } from './src/systems/CameraSystem';
 import Map from './src/renderers/Map';
 import { FlagSystem } from './src/systems/FlagSystem';
 import { GameStateSystem } from './src/systems/GameStateSystem';
 
-const {width: SCREENWIDTH, height: SCREENHEIGHT} = Dimensions.get('window'); //landscape
+const { width: SCREENWIDTH, height: SCREENHEIGHT } = Dimensions.get('window'); //landscape
 
-InitializeSocketIO();
-FindMatch();
 
 var GetEntities = () => {
-  console.log('getting entities');
-  let joystick = {
-    type: 'joystick',
-    // outerPosition: [SCREENWIDTH / 2, SCREENHEIGHT / 6 * 5],
-    outer_position: [75 + 60, SCREENHEIGHT - 45 - 60],
-    // outer_position: [30, SCREENHEIGHT - 30],
-    outer_radius: 60,
-    inner_position: [0, 0],
-    inner_radius: 30,
-    touch_id: null,
-    active: false,
-    angle: null,
-    renderer: Joystick,
-  };
+    let joystick = {
+        type: 'joystick',
+        // outerPosition: [SCREENWIDTH / 2, SCREENHEIGHT / 6 * 5],
+        outer_position: [75 + 60, SCREENHEIGHT - 45 - 60],
+        // outer_position: [30, SCREENHEIGHT - 30],
+        outer_radius: 60,
+        inner_position: [0, 0],
+        inner_radius: 30,
+        touch_id: null,
+        active: false,
+        angle: null,
+        renderer: Joystick,
+    };
 
-  let sprint_button = {
-    type: 'button',
-    id: 'sprint_button',
-    position: [SCREENWIDTH - 45 - 36, SCREENHEIGHT - 45 - 36],
-    radius: 36,
-    touch_id: null,
-    active: false,
-    renderer: Button,
-  };
-  
-  let action_button = {
-    type: 'button',
-    id: 'action_button',
-    position: [SCREENWIDTH - 45 - 36, SCREENHEIGHT - 45 - 36 - 36*2 - 20],
-    radius: 36,
-    touch_id: null,
-    active: false,
-    renderer: Button,
-  };
+    let sprint_button = {
+        type: 'button',
+        id: 'sprint_button',
+        position: [SCREENWIDTH - 45 - 36, SCREENHEIGHT - 45 - 36],
+        radius: 36,
+        touch_id: null,
+        active: false,
+        renderer: Button,
+    };
 
-  let camera = {
-    position: [0, 0],
-    target: null,
-  };
+    let action_button = {
+        type: 'button',
+        id: 'action_button',
+        position: [SCREENWIDTH - 45 - 36, SCREENHEIGHT - 45 - 36 - 36 * 2 - 20],
+        radius: 36,
+        touch_id: null,
+        active: false,
+        renderer: Button,
+    };
 
-  let map = {
-    position: [0, 0],
-    render_position: [0, 0],
-    renderer: Map,
-  };
+    let camera = {
+        position: [0, 0],
+        target: null,
+    };
 
-  let game = {
-      state: 'GAME_BEGIN'//WAITING, GAME_BEGIN , IN_PROGRESS, GAME_END
-  }
+    let map = {
+        position: [0, 0],
+        render_position: [0, 0],
+        renderer: Map,
+    };
 
-  let entities = {
-    map,    
-    sprint_button,
-    action_button,
-    joystick,
-    camera,
-    game
-  };
+    let game = {
+        state: 'GAME_BEGIN'//WAITING, GAME_BEGIN , IN_PROGRESS, GAME_END
+    }
 
-  // for(let i=0;i<1;i++)
-  // {
-  //     let player = players[i] || {
-  //         type: 'player',
-  //         position: [SCREENWIDTH/2, SCREENHEIGHT/2],
-  //         max_stamina: 100,
-  //         current_stamina: 100,
-  //         sprint_speed: 10,
-  //         default_speed: 4,
-  //         current_speed: 4,
-  //         renderer: Player
-  //     };
+    let entities = {
+        map,
+        sprint_button,
+        action_button,
+        joystick,
+        camera,
+        game
+    };
 
-  //     entities[`${i+2}`] = player;
-  // }
+    // for(let i=0;i<1;i++)
+    // {
+    //     let player = players[i] || {
+    //         type: 'player',
+    //         position: [SCREENWIDTH/2, SCREENHEIGHT/2],
+    //         max_stamina: 100,
+    //         current_stamina: 100,
+    //         sprint_speed: 10,
+    //         default_speed: 4,
+    //         current_speed: 4,
+    //         renderer: Player
+    //     };
 
-  return entities;
+    //     entities[`${i+2}`] = player;
+    // }
+
+    return entities;
 };
 
 let entities = GetEntities();
 
+const RenderGame = () => {
+    return <GameEngine
+        style={styles.container}
+        systems={[
+            FlagSystem,
+            JoystickSystem,
+            ButtonsSystem,
+            ControlsSystem,
+            PlayerSystem,
+            CameraSystem,
+            GameStateSystem
+        ]}
+        entities={entities}>
+        <StatusBar hidden={true} />
+    </GameEngine>;
+}
+
+const RenderMenu = (setGameInProgress, ip_address, setIPAddress) => {    
+    return <View style={{flex: 1, backgroundColor: 'gray', justifyContent: 'center', alignItems: 'center'}}>
+        <TextInput style={{width: 300, height: 40, backgroundColor: 'white', borderRadius: 12, marginBottom: 12, paddingLeft: 16}} onChangeText={(text)=>{
+            setIPAddress(text);
+        }}>
+            {ip_address}
+        </TextInput>
+        <TouchableOpacity style={{width: 300, height: 40, backgroundColor: '#3cc969', borderRadius: 12, justifyContent: 'center', alignItems: 'center'}} 
+        onPress={()=>{
+            InitializeSocketIO(ip_address);
+            FindMatch();
+            setGameInProgress(true);
+        }}>
+            <Text style={{fontWeight: '500', fontSize: 16, color:'white'}}>
+                FIND MATCH
+            </Text>
+        </TouchableOpacity>
+    </View>
+}
+
 const App = () => {
-  console.disableYellowBox = true;
-
-  // console.log(`players ${players}`);
-  // console.log(entities);
-
-  return (
-    <GameEngine
-      style={styles.container}
-      systems={[
-        FlagSystem,
-        JoystickSystem,
-        ButtonsSystem,
-        ControlsSystem,
-        PlayerSystem,
-        CameraSystem,
-        GameStateSystem
-      ]}
-      entities={entities}>
-      <StatusBar hidden={true} />
-    </GameEngine>
-  );
+    console.disableYellowBox = true;
+    
+    const [ip_address, setIPAddress] = useState('http://mooselliot.com:3000');
+    const [game_in_progress, setGameInProgress] = useState(false);
+    // console.log(`players ${players}`);
+    // console.log(entities);
+    if(game_in_progress) {
+        return RenderGame();
+    }
+    else {
+        return RenderMenu(setGameInProgress, ip_address, setIPAddress);
+    }     
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+    },
 });
 
 export default App;
--- src/managers/gamemanager.js
+++ src/managers/gamemanager.js
@@ -1,41 +1,47 @@
 import io from 'socket.io-client';
 import Player from '../renderers/Player';
 
-export var players = []; 
-export var flags = []; 
+export var players = [];
+export var flags = [];
 export var map = {};
 export var player_id = null;
 
-const server = 'http://localhost:3000';
-var socket = io(server);
+export var server = 'http://localhost:3000';
 
-export var InitializeSocketIO = () => {
-  socket.on('connect', () => console.log('connected to lobby'));
-  socket.on('JOIN_ROOM_CONFIRMED', namespace => JoinRoom(namespace));
+export var socket;
+
+export var InitializeSocketIO = (target_server) => {
+    console.log(`attempting to connect to ${target_server}...`)
+    server = target_server
+    socket = io(server);
+    socket.on('connect', () => console.log('connected to lobby'));
+    socket.on('JOIN_ROOM_CONFIRMED', namespace => JoinRoom(namespace));
 };
 
 export var FindMatch = () => {
-  socket.emit('REQUEST_FIND_MATCH');
+    if(socket) {
+        socket.emit('REQUEST_FIND_MATCH');
+    }
 };
 
 export var JoinRoom = namespace => {
-  socket = io(`${server}/${namespace}`);
-  socket.on('BIND_PLAYER', id => OnReceivePlayerBind(id));
-  socket.on('INIT_MAP', state => OnReceiveGameMap(state));
-  socket.on('GAME_STATE', state => OnReceiveGameState(state));
+    socket = io(`${server}/${namespace}`);
+    socket.on('BIND_PLAYER', id => OnReceivePlayerBind(id));
+    socket.on('INIT_MAP', state => OnReceiveGameMap(state));
+    socket.on('GAME_STATE', state => OnReceiveGameState(state));
 };
 
-export var OnReceivePlayerBind = (id)=>{        
+export var OnReceivePlayerBind = (id) => {
     player_id = id;
 }
-export var OnReceiveGameMap = (new_map)=>{        
+export var OnReceiveGameMap = (new_map) => {
     map = new_map;
 }
-export var OnReceiveGameState = (state)=>{            
-    players = state.players;        
+export var OnReceiveGameState = (state) => {
+    players = state.players;
     flags = state.flags;
 }
 
 export var SendControls = controls => {
-  socket.emit('CONTROLS', controls);
+    socket.emit('CONTROLS', controls);
 };
