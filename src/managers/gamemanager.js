a2c0badf661da9e125e17cf1d79dc554b694b506
Elliot Koh <appdev.moose@gmail.com>
2020-01-02 17:59:21 +0800

    map event key change

--- src/managers/gamemanager.js
+++ src/managers/gamemanager.js
@@ -19,7 +19,7 @@ export var FindMatch = () => {
 
 export var JoinRoom = namespace => {
   socket = io(`${server}/${namespace}`);
-  socket.on('MAP_DATA', state => OnReceiveGameMap(state));
+  socket.on('INIT_MAP', state => OnReceiveGameMap(state));
   socket.on('GAME_STATE', state => OnReceiveGameState(state));
 };
 
