def317ba8054b4ad7576a3ad4471b4d4aa41a697
Elliot Koh <appdev.moose@gmail.com>
2020-01-04 15:59:16 +0800

    Added color to scoreboard

--- src/renderers/Scoreboard.js
+++ src/renderers/Scoreboard.js
@@ -9,11 +9,11 @@ export default class Scoreboard extends PureComponent {
         let time = this.props.time;
 
         return (
-            <View style={[styles.container, { right: 12, top: 40, justifyContent: 'flex-end'}]}>   
+            <View style={[styles.container, { right: 12, top: 40,alignItems: 'flex-end'}]}>   
                 <View style={{justifyContent: 'flex-end', alignItems: 'center', flexDirection: 'row'}}>   
-                    <Text style={{fontFamily: 'Endless Boss Battle', fontSize: 32}}> {score[0]} </Text>
-                    <Text style={{fontFamily: 'Endless Boss Battle', fontSize: 32}}> vs </Text>
-                    <Text style={{fontFamily: 'Endless Boss Battle', fontSize: 32}}> {score[1]} </Text>
+                    <Text style={{fontFamily: 'Endless Boss Battle', fontSize: 32, color: Colors.lime_green}}> {score[0]} </Text>
+                    <Text style={{fontFamily: 'Endless Boss Battle', fontSize: 32, color: 'white'}}> vs </Text>
+                    <Text style={{fontFamily: 'Endless Boss Battle', fontSize: 32, color: Colors.red}}> {score[1]} </Text>
                 </View>
                 <Text style={{fontFamily: 'Endless Boss Battle', fontSize: 22, color: 'white'}}> {time} </Text>
             </View>
