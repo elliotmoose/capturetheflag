f98f2ed2bd4d747fbe696d6c063a328d548b8f0a
EYLeong <leong.enyi.1997@gmail.com>
2020-01-11 10:19:52 +0800

    announcements shadow

--- src/renderers/Announcements.js
+++ src/renderers/Announcements.js
@@ -12,6 +12,9 @@ export default class Announcements extends PureComponent {
         fontFamily: 'Endless Boss Battle',
         fontSize: UI.announcements.fontSize,
         color: 'white',
+        textShadowColor: 'rgba(0, 0, 0, 0.75)',
+        textShadowOffset: {width: -1, height: 1},
+        textShadowRadius: 10,
     }}>{message}</Text>
 
     })
